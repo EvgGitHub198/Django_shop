d35a50a4f92d5a424f18a8fef1f9a18f676ce6d1
Evgeny <97512743+EvgGitHub198@users.noreply.github.com>
2023-09-25 00:21:38 +0300

    Delete staticfiles directory

--- staticfiles/js/index.js
+++
@@ -1,9 +0,0 @@
-  $(document).ready(function() {
-    // Show dropdown menu on hover
-    $('.dropdown').hover(function() {
-      $(this).find('.dropdown-menu').addClass('show');
-    }, function() {
-      $(this).find('.dropdown-menu').removeClass('show');
-    });
-  });
-
